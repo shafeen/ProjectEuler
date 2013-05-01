9246e3ce3fcd0cf9224824cd4a8eeef929e5773f
Menkui <menkui@menkui-eeePC.(none)>
2013-05-01 11:22:04 -0400

    Added a summary of things to do for Prob#3

--- 003/prime.cpp
+++ 003/prime.cpp
@@ -3,9 +3,17 @@ using namespace std;
 
 int main()
 {	
+	typedef long long superlong;
+	superlong test =  600851475143;
+	cout << test << endl;
+
+	/*
+		Given a number ---> find all its factors
+		For each factor --> find out if its prime 
+		If it's prime ----> count it as a prime facto
+	*/
 
 
 
-
-	return 1
+	return 1;
 }
