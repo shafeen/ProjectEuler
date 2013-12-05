6079e319bbe6decce4043f4489ea17c2b3ce3b09
Shafeen M <sodl02@gmail.com>
2013-12-05 01:51:01 -0500

    Solved problem 5

---
+++ 005/divisibleByAll.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+typedef unsigned long long superlong;
+
+std::mutex cout_lock;
+
+
+bool isDivisibleByAll(superlong number)
+{
+	for(uint i = 1; i <= 20; i++)
+		if(number % i != 0)
+			return false;
+
+	return true;
+}
+
+void problem5() // brute force
+{
+
+	superlong number = 20;
+	
+	while(true)
+	{
+		if(isDivisibleByAll(number++))
+			break;
+
+	}
+
+	
+	cout << number-1 << " is divisible by each of 1 thru 20!\n";
+}
+
+
+int main()
+{	
+
+	problem5();
+
+	return 0;
+}
+
+
+
+
+
