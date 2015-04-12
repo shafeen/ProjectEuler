faa55ff59abf6eb61ed9665cb119019913bf8897
Shafeen M <sodl02@gmail.com>
2015-04-12 00:38:01 -0400

    cleaned up problem 5

--- 005/divisibleByAll.cpp
+++ 005/divisibleByAll.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 typedef unsigned long long superlong;
 
-std::mutex cout_lock;
-
-
 bool isDivisibleByAll(superlong number)
 {
 	for(uint i = 1; i <= 20; i++)
@@ -20,17 +17,13 @@ bool isDivisibleByAll(superlong number)
 
 void problem5() // brute force
 {
-
 	superlong number = 20;
 	
 	while(true)
 	{
 		if(isDivisibleByAll(number++))
 			break;
-
 	}
-
-	
 	cout << number-1 << " is divisible by each of 1 thru 20!\n";
 }
 
